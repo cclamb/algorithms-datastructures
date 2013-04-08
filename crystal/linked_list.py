c6417a19f33cc534cb7d414b560ca388604bb0e8
Chris lamb <cclamb@ece.unm.edu>
2013-04-08 14:28:32 -0600

    queue added as well as specific pycharm config exclusions in gitignore

--- crystal/linked_list.py
+++ crystal/linked_list.py
@@ -1,3 +1,5 @@
+from pdb import _rstr
+
 __author__ = 'cclamb'
 
 
@@ -7,48 +9,64 @@ def create_list(type):
     else:
         raise NameError("unknown stack type requested")
 
+
 class ListType:
-    LinkedList  = range(1)
+    LinkedList = range(1)
+
+
+class _Record:
+
+    class Type:
+        Sentinel, Standard = range(2)
+
+    def __init__(self, prev=None, next=None, key=None, record_type=Type.Standard):
+        self.prev = prev
+        self.next = next
+        self.key = key
+        self.record_type = record_type
 
 
 class LinkedList:
-
-    class _Record:
-        def __init__(self, prev = None, next = None, key = None):
-            self.prev = prev
-            self.next = next
-            self.key = key
 
     def __init__(self):
         self._head = None
 
     def search(self, key):
         x = self._head
-        while(x != None and x.key != key):
+        while x is not None and x.key != key:
             x = x.next
         return x
 
     def insert(self, key):
-        record = self._Record(self._head, None, key)
+        record = _Record(self._head, None, key)
         x = self._head
 
-        if (x == None):
+        if x is None:
             self._head = record
             return self
 
-        while (x.next != None):
+        while x.next is not None:
             x = x.next
 
         x.next = record
         return self
 
     def delete(self, key):
-        if (self._head == None):
+        if self._head is None:
             return self
         x = self._head
-        while (x != None and x.key != key):
+        while x is not None and x.key != key:
             x = x.next
-        prev = x.prev
-        next = x.next
-        prev.next = next
+        prev_record = x.prev
+        next_record = x.next
+        prev_record.next = next_record
         return self
+
+
+class SentinelLinkedList:
+
+    def __init__(self):
+        head = _Record(None, None, None, _Record.Type.Sentinel)
+        tail = _Record(head, None, None, _Record.Type.Sentinel)
+        head.next = tail
+        self._head = [head, tail]
