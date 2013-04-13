abaa85f43601a4786db2ba93a9e55c209c29a560
cclamb <chrislambistan@gmail.com>
2013-04-13 09:49:57 -0600

    linked list tests complete

--- crystal/hash_table.py
+++ crystal/hash_table.py
@@ -1,7 +1,54 @@
 __author__ = 'cclamb'
 
 
-class DirectAddressTable:
+from crystal.linked_list import LinkedList
+from crystal.linked_list import ListType
+from crystal.linked_list import create_list
 
-    def __init__(self):
+
+class ChainedHashTable:
+
+
+    class _Record:
+
+        def __init__(self, k, v):
+            self.key = k
+            self.value = v
+
+
+    Length = 256
+
+
+    def __init_(self):
+        self._store = []
+        for i in range(ChainedHashTable.Length):
+            self._store[i] = None
+
+    def insert(self, k, v):
+        key = self._generate_key(k)
+
+        if self._is_slot_empty():
+            self._insert_list(key)
+
+        record = ChainedHashTable._Record(k, v)
+        self._store[key].insert(record)
+
+        return self
+
+    def search(self):
         pass
+
+    def delete(self):
+        pass
+
+    def _generate_key(self, k):
+        if type(k) is not str:
+            raise TypeError('the submitted key is not a string')
+        return bytes(k, 'utf-8')[0]
+
+
+    def _insert_list(self, key):
+        self._store[key] = LinkedList()
+
+    def _is_slot_empty(self, idx):
+        return self._store(idx) is None
--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -2,8 +2,20 @@ __author__ = 'cclamb'
 
 import unittest
 
+from crystal.hash_table import ChainedHashTable
 
-class TestDirectAddressTable(unittest.TestCase):
+class TestChainedHashTable(unittest.TestCase):
 
     def test_create(self):
+        self.assertIsNotNone(ChainedHashTable())
+
+    def test_generate_key(self):
+        h = ChainedHashTable()
+        self.assertEquals(bytes('foo', 'utf-8')[0], h._generate_key('foo'))
+        self.assertRaises(TypeError, h._generate_key, 1)
+
+    def test_insert_list(self):
+        pass
+
+    def test_is_slot_empty(self):
         pass
