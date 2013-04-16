12d7d3ca04a45895b4fc77dce3a8b17f76471017
cclamb <cclamb@cs.unm.edu>
2013-04-16 06:22:44 -0600

    Pushing new .gitignore and new hash tests

--- crystal/hash_table.py
+++ crystal/hash_table.py
@@ -8,26 +8,23 @@ from crystal.linked_list import create_list
 
 class ChainedHashTable:
 
-
     class _Record:
 
         def __init__(self, k, v):
             self.key = k
             self.value = v
 
-
     Length = 256
 
-
-    def __init_(self):
+    def __init__(self):
         self._store = []
-        for i in range(ChainedHashTable.Length):
-            self._store[i] = None
+        for i in range(256):
+            self._store.insert(0, None)
 
     def insert(self, k, v):
         key = self._generate_key(k)
 
-        if self._is_slot_empty():
+        if self._is_slot_empty(key):
             self._insert_list(key)
 
         record = ChainedHashTable._Record(k, v)
@@ -44,11 +41,10 @@ class ChainedHashTable:
     def _generate_key(self, k):
         if type(k) is not str:
             raise TypeError('the submitted key is not a string')
-        return bytes(k, 'utf-8')[0]
-
+        return int(bytes(k, 'utf-8')[0])
 
     def _insert_list(self, key):
         self._store[key] = LinkedList()
 
     def _is_slot_empty(self, idx):
-        return self._store(idx) is None
+        return self._store[idx] is None
--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -12,11 +12,15 @@ class TestChainedHashTable(unittest.TestCase):
 
     def test_generate_key(self):
         h = ChainedHashTable()
-        self.assertEquals(bytes('foo', 'utf-8')[0], h._generate_key('foo'))
+        self.assertEquals(int(bytes('foo', 'utf-8')[0]), h._generate_key('foo'))
         self.assertRaises(TypeError, h._generate_key, 1)
 
     def test_insert_list(self):
-        pass
+        h = ChainedHashTable()
+        h._insert_list(3)
+        h._store[3].insert(4)
+        x = h._store[3].search(4)
+        self.assertTrue(x.key, 4)
 
     def test_is_slot_empty(self):
         pass
